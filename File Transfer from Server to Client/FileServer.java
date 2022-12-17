607c13d4d6736a3f2138ddf66c6fda83115447f2
Vijaysankar-R <105067556+Vijaysankar-R@users.noreply.github.com>
2022-12-17 11:17:01 +0530

    Add files via upload

---
+++ File Transfer from Server to Client/FileServer.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+public class FileServer
+{
+public static void main (String [] args ) throws IOException 
+{
+ServerSocket servsock = new ServerSocket(13267); 
+while (true)
+{
+System.out.println("Waiting..."); 
+Socket sock =servsock.accept();
+ System.out.println("Acceptedconnection : " + sock); 
+File myFile = new File("1.pdf");
+byte [] mybytearray = new byte [(int)myFile.length()];
+FileInputStream fis = new FileInputStream(myFile);
+BufferedInputStream bis = new BufferedInputStream(fis);
+bis.read(mybytearray,0,mybytearray.length);
+OutputStream os = sock.getOutputStream();
+System.out.println("Sending...");
+os.write(mybytearray,0,mybytearray.length);
+os.flush();
+sock.close();
+}
+}
+}
